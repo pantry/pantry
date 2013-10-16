2d34b1ed9beadf43cee3c65d5229440e6ec74b3c
Jason Roelofs <jasongroelofs@gmail.com>
2013-10-16 18:12:29 -0400

    Set up socket communication between Server / Client once for the suite

--- test/acceptance/basic_communication_test.rb
+++ test/acceptance/basic_communication_test.rb
@@ -2,48 +2,63 @@ require 'acceptance/test_helper'
 
 describe "Basic Server Client Communication" do
 
+  # We are dealing with actual socket communication here, so we want
+  # to set up the socket communication itself once then play with various
+  # ways we communicate over these sockets.
+  def self.setup_environment
+    if $basic_server_client_comm_setup.nil?
+      Celluloid.boot
+
+      Pantry.config.server_host = "127.0.0.1"
+      Pantry.config.pub_sub_port = 10101
+
+      @server = Pantry::Server.new
+      @server.run
+
+      @client1 = Pantry::Client.new
+      @client1.run
+
+      @client2 = Pantry::Client.new
+      @client2.run
+
+      # Ensure communication figures itself out in time
+      sleep 1
+
+      Minitest.after_run do
+        @client1.shutdown
+        @client2.shutdown
+        @server.shutdown
+      end
+
+      $basic_server_client_comm_setup = true
+    end
+
+    [@server, @client1, @client2]
+  end
+
   before do
-    Celluloid.boot
+    @server, @client1, @client2 = self.class.setup_environment
   end
 
   describe "Server" do
     it "can give a message to all connected clients" do
-      Pantry.config.server_host = "127.0.0.1"
-      Pantry.config.pub_sub_port = 10101
-
-      server = Pantry::Server.new
-      server.run
-
-      client1 = Pantry::Client.new
-      client1.run
-
-      client2 = Pantry::Client.new
-      client2.run
-
       client1_test_message = false
-      client1.on(:test_message) do
+      @client1.on(:test_message) do |message|
         client1_test_message = true
       end
 
       client2_test_message = false
-      client2.on(:test_message) do
+      @client2.on(:test_message) do |message|
         client2_test_message = true
       end
 
-      # Ensure clients are registered
-      sleep 1
-
-      server.publish_to_clients("test_message")
+      @server.publish_to_clients("test_message")
 
       # Give communication time to happen
       sleep 1
 
       assert client1_test_message, "Client 1 did not get the message"
       assert client2_test_message, "Client 2 did not get the message"
-
-      client1.shutdown
-      client2.shutdown
-      server.shutdown
     end
 
     it "can give a message to a subset of all connected clients"
