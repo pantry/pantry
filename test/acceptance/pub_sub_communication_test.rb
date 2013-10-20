f45cbe3d1851bce37936db942aaeb9dec3d80e91
Jason Roelofs <jasongroelofs@gmail.com>
2013-10-20 18:08:22 -0400

    Move acceptance test setup into the test helper
    All acceptance tests will use the same server and clients, which due to
    socket usage need to be set up just once for the whole suite.

--- test/acceptance/pub_sub_communication_test.rb
+++ test/acceptance/pub_sub_communication_test.rb
@@ -2,40 +2,6 @@ require 'acceptance/test_helper'
 
 describe "Pub/Sub Communication" do
 
-  # We are dealing with actual socket communication here, so we want
-  # to set up the socket communication itself once then play with various
-  # ways we communicate over these sockets.
-  def self.setup_environment
-    if $basic_server_client_comm_setup.nil?
-      Celluloid.boot
-
-      Pantry.config.server_host = "127.0.0.1"
-      Pantry.config.pub_sub_port = 10101
-
-      @server = Pantry::Server.new
-      @server.run
-
-      @client1 = Pantry::Client.new
-      @client1.run
-
-      @client2 = Pantry::Client.new
-      @client2.run
-
-      # Ensure communication figures itself out in time
-      sleep 1
-
-      Minitest.after_run do
-        @client1.shutdown
-        @client2.shutdown
-        @server.shutdown
-      end
-
-      $basic_server_client_comm_setup = true
-    end
-
-    [@server, @client1, @client2]
-  end
-
   before do
     @server, @client1, @client2 = self.class.setup_environment
   end
--- test/acceptance/test_helper.rb
+++ test/acceptance/test_helper.rb
@@ -2,3 +2,42 @@ require 'minitest/autorun'
 require 'pantry/server'
 require 'pantry/client'
 require 'support/matchers'
+
+class Minitest::Test
+
+  # We are dealing with actual socket communication here, so we want
+  # to set up the socket communication itself once then play with various
+  # ways we communicate over these sockets.
+  def self.setup_environment
+    if $basic_server_client_comm_setup.nil?
+      Celluloid.boot
+
+      Pantry.config.server_host = "127.0.0.1"
+      Pantry.config.pub_sub_port = 10101
+      Pantry.config.receive_port = 10102
+
+      $server = Pantry::Server.new
+      $server.run
+
+      $client1 = Pantry::Client.new
+      $client1.run
+
+      $client2 = Pantry::Client.new
+      $client2.run
+
+      # Ensure communication figures itself out in time
+      sleep 1
+
+      Minitest.after_run do
+        $client1.shutdown
+        $client2.shutdown
+        $server.shutdown
+      end
+
+      $basic_server_client_comm_setup = true
+    end
+
+    [$server, $client1, $client2]
+  end
+
+end
