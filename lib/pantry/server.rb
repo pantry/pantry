655bcb2bde3d3fd8079de5194a76a7407e9fa3ed
Jason Roelofs <jasongroelofs@gmail.com>
2013-10-20 17:09:02 -0400

    Servers can receive messages directly from Clients
    This receive path has two parts: a wait list, which is a "wait for a
    response to a message I previously sent", and a regular handler for when
    clients are requesting data from the Server.

--- lib/pantry/server.rb
+++ lib/pantry/server.rb
@@ -1,11 +1,18 @@
 require 'pantry/config'
 require 'pantry/communication/message'
+require 'pantry/communication/message_filter'
 require 'pantry/communication/publish_socket'
+require 'pantry/communication/receive_socket'
+require 'pantry/communication/wait_list'
 
 module Pantry
 
   # The Pantry Server
   class Server
+
+    def initialize
+      @response_wait_list = Communication::WaitList.new
+    end
 
     # Start up the networking stack and start the server
     def run
@@ -14,17 +21,44 @@ module Pantry
         Pantry.config.pub_sub_port
       )
       @publish_socket.open
+
+      @receive_socket = Communication::ReceiveSocket.new(
+        Pantry.config.server_host,
+        Pantry.config.receive_port
+      )
+      @receive_socket.add_listener(self)
+      @receive_socket.open
     end
 
-    # Broadcast a message to all clients, optionally filtering for certain
-    # clients.
+    # Broadcast a message to all clients, optionally filtering for certain clients.
     def publish_to_clients(message, filter = nil)
       @publish_socket.send_message(message, filter)
+    end
+
+    # Send a message to the Client(s) with the given identity.
+    # Returns a Future object, use #value to get the response from the Client
+    # when it's available.
+    def request_from_client(client_identity, message)
+      publish_to_clients(
+        message, Communication::MessageFilter.new(:identity => client_identity)
+      )
+
+      @response_wait_list.wait_for(client_identity, message)
+    end
+
+    def handle_message(message)
+      puts "Server got message #{message}"
+      if @response_wait_list.waiting_for?(message)
+        @response_wait_list.received(message)
+      else
+        # Process our own messages
+      end
     end
 
     # Close down the networking and clean up resources
     def shutdown
       @publish_socket.close
+      @receive_socket.close
     end
 
   end
--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -6,6 +6,9 @@ describe Pantry::Server do
   before do
     Celluloid.init
     Pantry::Communication::PublishSocket.any_instance.stubs(:open)
+
+    Pantry::Communication::ReceiveSocket.any_instance.stubs(:add_listener)
+    Pantry::Communication::ReceiveSocket.any_instance.stubs(:open)
   end
 
   it "opens a publish socket for communication, closing it on shutdown" do
@@ -13,6 +16,17 @@ describe Pantry::Server do
     Pantry::Communication::PublishSocket.any_instance.expects(:close)
 
     server = Pantry::Server.new
+    server.run
+    server.shutdown
+  end
+
+  it "opens a receive socket for communication, closing it on shutdown" do
+    server = Pantry::Server.new
+
+    Pantry::Communication::ReceiveSocket.any_instance.expects(:add_listener).with(server)
+    Pantry::Communication::ReceiveSocket.any_instance.expects(:open)
+    Pantry::Communication::ReceiveSocket.any_instance.expects(:close)
+
     server.run
     server.shutdown
   end
@@ -36,4 +50,17 @@ describe Pantry::Server do
     server.publish_to_clients("message", filter)
   end
 
+  it "sends a message to a single client via identity, returning a future" do
+    server = Pantry::Server.new
+    server.run
+
+    filter = Pantry::Communication::MessageFilter.new(identity: "client1")
+    message = Pantry::Communication::Message.new("message")
+    Pantry::Communication::PublishSocket.any_instance.expects(:send_message).with(message, filter)
+
+    future = server.request_from_client("client1", message)
+
+    assert_not_nil future
+    assert_not future.ready?
+  end
 end
