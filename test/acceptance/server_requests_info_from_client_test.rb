b7cc96d36ebbba35ac26e67ff0ead8b1efe4c916
Jason Roelofs <jasongroelofs@gmail.com>
2013-10-21 14:12:19 -0400

    Add test to ensure multiple clients handle their own responses

--- test/acceptance/server_requests_info_from_client_test.rb
+++ test/acceptance/server_requests_info_from_client_test.rb
@@ -20,8 +20,23 @@ describe "Server requests info from the Client" do
     end
   end
 
-  it "asks multiple clients for info and matches responses with requests"
+  it "asks multiple clients for info and matches responses with requests" do
+    @client1.on(:request_message) do |message|
+      "Client 1 responds"
+    end
 
-  it "receives information from all clients with the same identity"
+    @client2.on(:request_message) do |message|
+      "Client 2 responds"
+    end
+
+    message = Pantry::Communication::Message.new("request_message")
+    future1 = @server.send_request(@client1.identity, message)
+    future2 = @server.send_request(@client2.identity, message)
+
+    Timeout::timeout(1) do
+      assert_equal ["Client 1 responds"], future1.value.body
+      assert_equal ["Client 2 responds"], future2.value.body
+    end
+  end
 
 end
