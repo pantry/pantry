6bada1441f56d807d0dee9416b09ceda0a57b8ca
Jason Roelofs <jasongroelofs@gmail.com>
2013-10-23 16:17:46 -0400

    Add test that client -> server requests are handled in order

--- test/acceptance/client_requests_info_from_server_test.rb
+++ test/acceptance/client_requests_info_from_server_test.rb
@@ -20,4 +20,23 @@ describe "Client requests information from the Server" do
     end
   end
 
+  it "handles multiple requests in the proper order" do
+    response_count = 0
+    @server.on(:client_info) do |message|
+      "Server responds #{response_count += 1}"
+    end
+
+    message = Pantry::Communication::Message.new("client_info")
+    futures = []
+    10.times do
+      futures << @client1.send_request(message)
+    end
+
+    Timeout::timeout(5) do
+      futures.each_with_index do |future, idx|
+        assert_equal ["Server responds #{idx + 1}"], future.value.body
+      end
+    end
+  end
+
 end
