6b622b57c6a08381a1e6db0adfd7d6c67214e56d
Jason Roelofs <jasongroelofs@gmail.com>
2013-11-11 16:38:22 -0500

    Update ClientRegistry to keep track of when a client checked in
    This also fixes an issue, tested in an upcoming acceptance test on
    heartbeats, to where the Set wasn't seeing that client1 and client1 were
    the same, due to them being Celluloid::ActorProxies. Now keying off of
    the identity.

--- lib/pantry/client_registry.rb
+++ lib/pantry/client_registry.rb
@@ -5,22 +5,27 @@ module Pantry
   class ClientRegistry
 
     def initialize
-      @clients = Set.new
+      clear!
     end
 
     # Return all known clients
     def all
-      @clients.to_a
+      @clients.map {|identity, record| record.client }
+    end
+
+    # Clear out the registry entirely
+    def clear!
+      @clients = Hash.new {|hash, key| hash[key] = ClientRecord.new }
     end
 
     # Check in a client
     def check_in(client)
-      @clients << client
+      @clients[client.identity].check_in(client)
     end
 
     # Has the given client checked in?
     def include?(client)
-      @clients.include?(client)
+      @clients[client.identity].checked_in?
     end
 
     # Find and return all clients who will receive messages
@@ -28,13 +33,41 @@ module Pantry
     def all_matching(stream_or_filter)
       case stream_or_filter
       when String
-        @clients.select do |client|
+        select_clients_matching do |client|
           client.filter.matches?(stream_or_filter)
         end
       else
-        @clients.select do |client|
+        select_clients_matching do |client|
           stream_or_filter.includes?(client.filter)
         end
+      end
+    end
+
+    protected
+
+    def select_clients_matching
+      selected_records = @clients.select do |identity, record|
+        yield(record.client)
+      end
+
+      selected_records.values.map(&:client)
+    end
+
+    class ClientRecord
+      attr_reader :client
+
+      def initialize
+        @client = nil
+        @last_checked_in_at = nil
+      end
+
+      def check_in(client)
+        @client = client
+        @last_checked_in_at = Time.now
+      end
+
+      def checked_in?
+        !@last_checked_in_at.nil?
       end
     end
 
