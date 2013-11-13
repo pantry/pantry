375990a8280987574f468a8b5c990445991d0399
Jason Roelofs <jasongroelofs@gmail.com>
2013-11-13 16:06:02 -0500

    Keep a global list of the commands Clients and Servers know about

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -40,3 +40,63 @@ require 'pantry/client_registry'
 require 'pantry/client'
 require 'pantry/server'
 require 'pantry/cli'
+
+
+module Pantry
+
+  # Register a command object class for the commands system.
+  # This will register the command object as handleable by both the Server and a Client.
+  def add_command(command_class)
+    add_client_command(command_class)
+    add_server_command(command_class)
+  end
+
+  # Register a command object class to be handled only by Clients
+  def add_client_command(command_class)
+    ensure_proper_command_class(command_class)
+    check_for_duplicates(client_commands, command_class)
+
+    client_commands << command_class
+  end
+
+  # Register a command object class to be handled only by the Server
+  def add_server_command(command_class)
+    ensure_proper_command_class(command_class)
+    check_for_duplicates(server_commands, command_class)
+
+    server_commands << command_class
+  end
+
+  # Return the list of known Client command classes
+  def client_commands
+    @client_commands ||= []
+  end
+
+  # Return the list of known Server command classes
+  def server_commands
+    @server_commands ||= []
+  end
+
+  def ensure_proper_command_class(command_class)
+    unless command_class.is_a?(Class)
+      raise Pantry::InvalidCommandError.new("Expected a Class, got an #{command_class.class}")
+    end
+
+    unless command_class.ancestors.include?(Pantry::Command)
+      raise Pantry::InvalidCommandError.new("Expected a class that's a subclass of Pantry::Command")
+    end
+  end
+
+  def check_for_duplicates(command_list, command_class_to_add)
+    known_commands = command_list.map(&:command_type)
+    if known_commands.include?(command_class_to_add.command_type)
+      raise Pantry::DuplicateCommandError.new("Command with type #{command_class_to_add.command_type} already registered")
+    end
+  end
+
+  class InvalidCommandError < Exception; end
+
+  class DuplicateCommandError < Exception; end
+
+  extend self
+end
---
+++ test/unit/pantry_test.rb
@@ -0,0 +1,60 @@
+require 'unit/test_helper'
+
+describe Pantry do
+
+  class TestCommand < Pantry::Command
+  end
+
+  before do
+    Pantry.client_commands.clear
+    Pantry.server_commands.clear
+  end
+
+  describe "#add_command" do
+    it "registers a command class for client and server to handle" do
+      Pantry.add_command(TestCommand)
+
+      assert_equal [TestCommand], Pantry.client_commands
+      assert_equal [TestCommand], Pantry.server_commands
+    end
+
+    it "errors if the given class is not a Pantry::Command" do
+      assert_raises(Pantry::InvalidCommandError) do
+        Pantry.add_command(Object)
+      end
+    end
+
+    it "errors if was given an object that isn't a Class" do
+      assert_raises(Pantry::InvalidCommandError) do
+        Pantry.add_command(Object.new)
+      end
+    end
+
+    it "warns if the given Command conflicts name-wise with an already registered command" do
+      Pantry.add_command(TestCommand)
+
+      assert_raises(Pantry::DuplicateCommandError) do
+        Pantry.add_command(TestCommand)
+      end
+    end
+  end
+
+  describe "#add_client_command" do
+    it "adds commands only for the client to handle" do
+      Pantry.add_client_command(TestCommand)
+
+      assert_equal [TestCommand], Pantry.client_commands
+      assert_equal [], Pantry.server_commands
+    end
+  end
+
+  describe "#add_server_command" do
+    it "adds commands only for the server to handle" do
+      Pantry.add_server_command(TestCommand)
+
+      assert_equal [], Pantry.client_commands
+      assert_equal [TestCommand], Pantry.server_commands
+    end
+  end
+
+end
