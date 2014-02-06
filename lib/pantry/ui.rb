3550a35b38a4abb7db5d69c7812b15cd14030f74
Jason Roelofs <jasongroelofs@gmail.com>
2014-02-06 15:05:55 -0500

    UI can ask the user to continue, waiting for input

--- lib/pantry/ui.rb
+++ lib/pantry/ui.rb
@@ -15,13 +15,23 @@ module Pantry
       @highline = HighLine.new
     end
 
+    # Send a message to STDOUT
     def say(message)
       @highline.say(message)
     end
 
+    # Print out a list, attempting to make it look somewhat reasonable
     def list(array)
       array.each do |entry|
         say([entry].flatten.join(" -- "))
+      end
+    end
+
+    # Show the user a message and ask them to continue by hitting Enter,
+    # or they can cancel with "No"
+    def continue?(message)
+      @highline.agree(message) do |q|
+        q.default = "yes"
       end
     end
 
